7d2d3d3cd138f89d0975eef00ca6d530bba49fd9
Joel Warburton <joel.s.warburton@gmail.com>
2021-12-21 19:55:00 +1100

    complete day 2 part 2

--- day2.py
+++ day2.py
@@ -6,18 +6,45 @@ depth = 0
 position = 0
 
 
-
-
 for line in inputLines:
-    line = line.split()
-    print(line)
-    if(line[0] == "up"):
-        depth -= int(line[1])
-    elif(line[0]== "down"):
-        depth += int(line[1])
+    command, amount = line.split()
+    amount = int(amount)
+  
+    if(command == "up"):
+        depth -= amount
+    elif(command == "down"):
+        depth += amount
     else: 
-        position += int(line[1])
+        position += amount
 
 print(depth)
 print(position)
 print(depth * position)
+
+
+#part 2
+
+input = open("/home/joel/IdeaProjects/advent/src/input2.txt", "r")
+inputLines = [data.strip() for data in input.readlines()]
+
+depth = 0
+position = 0
+aim = 0
+
+
+for line in inputLines:
+    command, amount = line.split()
+    amount = int(amount)
+  
+    if(command == "up"):
+        aim -= amount
+    elif(command == "down"):
+        aim += amount
+    else: 
+        position += amount
+        depth += aim * amount
+
+print(depth)
+print(position)
+print(depth * position)
+
